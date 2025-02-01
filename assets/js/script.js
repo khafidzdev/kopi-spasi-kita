228aad330ed7eb481d317cbb38939d7a214a604e
fristkhfdz22 <khafidzrizziqalhuda@gmail.com>
2025-02-01 09:23:38 -0800

    u

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 document.getElementById("menuBtn").addEventListener("click", function() {
     document.body.classList.toggle("no-scroll");
+    document.getElementById("menuOverlay").classList.toggle("active");
+    document.querySelector(".menu-btn").classList.toggle("active");
 });
 
 document.getElementById("menuOverlay").addEventListener("click", function() {
     document.body.classList.remove("no-scroll");
+    document.getElementById("menuOverlay").classList.remove("active");
+    document.querySelector(".menu-btn").classList.remove("active");
 });
