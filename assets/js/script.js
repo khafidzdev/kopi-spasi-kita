9405f5fecc3e61c62ec48a79f2de5322f82f4e20
khafidzdev <khafidzrizziq@gmail.com>
2025-03-14 00:46:45 +0700

    u

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
-function toggleMenu() {
-    let menu = document.getElementById("overlay");
-    let btn = document.querySelector(".menu-btn");
+document.getElementById("menuBtn").addEventListener("click", function() {
+    //document.body.classList.toggle("no-scroll");//
+    document.getElementById("menuOverlay").classList.toggle("active");
+    document.querySelector(".menu-btn").classList.toggle("active");
+});
 
-    menu.classList.toggle("active");
-    btn.classList.toggle("active");
-}
+document.getElementById("menuOverlay").addEventListener("click", function() {
+    //document.body.classList.remove("no-scroll");//
+    document.getElementById("menuOverlay").classList.remove("active");
+    document.querySelector(".menu-btn").classList.remove("active");
+});
+const foodLink = document.getElementById('foodLink');
+const drinkLink = document.getElementById('drinkLink');
+const foodMenu = document.getElementById('foodMenu');
+const drinkMenu = document.getElementById('drinkMenu');
+
+foodLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    foodMenu.style.display = 'block';
+    drinkMenu.style.display = 'none';
+});
+
+drinkLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    foodMenu.style.display = 'none';
+    drinkMenu.style.display = 'block';
+});
